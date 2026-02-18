2d606fde4f7eecb85170ba5b3b00780251a4c64b
Chraeten <chraetenlategan@gmail.com>
2026-02-18 22:48:05 +0200

    Refactor Pong game setup and collision detection

--- Day22 main.py
+++ Day22 main.py
@@ -1,7 +1,52 @@
 from turtle import Turtle,Screen
-
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+#do the screen
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width = 600,height = 600)
-screen.title("Pong")
+screen.setup(width=900, height=600)
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up,"i")
+screen.onkey(r_paddle.go_down,"k")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(ball.move_speed)
+    #detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    ball.move()
+
+    #detect collision
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #detect if R paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.r_point()
+        scoreboard.update_scoreboard()
+    
+        #detect if R paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.l_point()
+        scoreboard.update_scoreboard()
+
+
 screen.exitonclick()
