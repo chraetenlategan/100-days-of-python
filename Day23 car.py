2b78542d38a74c99c12c9a16ef48b7c6fe74f0a4
Chraeten <chraetenlategan@gmail.com>
2026-02-19 19:34:07 +0200

    Add random color selection for Car class

--- Day23 car.py
+++ Day23 car.py
@@ -1,9 +1,10 @@
 from turtle import Turtle
-
+import random
 class Car(Turtle):
 
+    colors = ["red", "blue", "green", "orange", "purple", "gold", "cyan", "hotpink"]
     def __init__(self):
         super().__init__()
         self.shape("square")
-        self.color("red")
+        self.color(random.choice(self.colors))
         self.shapesize(stretch_wid = 1, stretch_len =3)
