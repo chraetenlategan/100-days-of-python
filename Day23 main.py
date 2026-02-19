50738c0bb1f2dc71d75fac88acb2033ea84867a8
Chraeten <chraetenlategan@gmail.com>
2026-02-19 19:33:39 +0200

    Create and position 8 cars on the screen
    Added functionality to create and position multiple cars.

--- Day23 main.py
+++ Day23 main.py
@@ -1,13 +1,35 @@
 from turtle import Turtle,Screen
 from car import Car
+import random
 
+#Screen Setup
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=1000, height=800)
 screen.tracer(0)
 
-vroom = Car()
+#Create 8 cars
+cars = [Car() for _ in range(8)]
+# The possible fix position for cars
+car_positions = [
+    (-500, -400), 
+    (-500, -300), 
+    (-500, -200), 
+    (-500, -100), 
+    (-500, 0), 
+    (-500, 100), 
+    (-500, 200), 
+    (-500, 300), 
+    (-500, 400)
+]
+
+
+for i in range (len(cars)):
+    car.goto(car_positions[i-1])
+
+
+
+
+
 screen.update()
-
-
 screen.exitonclick()
