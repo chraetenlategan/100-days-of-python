25b0c7bd1fc3b82795344aef5b37c5a697cd6ebf
Chraeten <chraetenlategan@gmail.com>
2026-02-20 21:50:14 +0200

    Implement car spawning logic in Day23 car.py
    Added a spawn method to position cars randomly on the screen.

--- Day23 car.py
+++ Day23 car.py
@@ -8,3 +8,26 @@ class Car(Turtle):
         self.shape("square")
         self.color(random.choice(self.colors))
         self.shapesize(stretch_wid = 1, stretch_len =3)
+        self.penup()
+    
+    def spawn(self, screen, index, total_cars):
+        margin = 50
+        playable_height = screen.window_height() - (margin * 2)
+        starting_y = -(screen.window_height() / 2) + margin
+        
+        spacing = playable_height / total_cars
+        
+        left_edge = -(screen.window_width() / 2) + margin
+        right_edge = (screen.window_width() / 2) - margin
+
+   
+        new_y = starting_y + (index * spacing)
+        random_x = random.choice([left_edge, right_edge])
+        self.goto(x=random_x, y=new_y)
+        
+        if random_x == right_edge:
+            self.setheading(180)
+        else:
+            self.setheading(0)
+
+        
