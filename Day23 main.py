d523042f6aee8f874a99adf68de93188c34c3384
Chraeten <chraetenlategan@gmail.com>
2026-02-22 11:25:02 +0200

    Refactor car creation and add scoreboard

--- Day23 main.py
+++ Day23 main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle,Screen
 from car import Car
 from timmy import Timmy
+from scoreboard import Scoreboard
 import random,time
 
 #Screen Setup
@@ -9,13 +10,16 @@ screen.bgcolor("black")
 screen.setup(width=1000, height=800)
 screen.tracer(0)
 
-#Create 8 cars
-cars = [Car() for _ in range(8)]
+#Create 8 car
+car_1 = [Car() for _ in range(random.randint(6,10))]
 
-for i in range(len(cars)):
-    cars[i].spawn(screen, i, len(cars))
+for i in range(len(car_1)):
+    car_1[i].spawn(screen, i, len(car_1))
+
+
 
 tim = Timmy()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(tim.move_up,"w")
@@ -28,9 +32,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for car in cars:
+    for car in car_1:
         car.forward(5)
-
-
+        car.return_position()
+        if car.distance(tim) < 25:
+            game_is_on = False
+            scoreboard.collision()
+    
 
 screen.exitonclick()
