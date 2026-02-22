a674ad0d7f0afb869dd33cebb2d780489b08b1fa
Chraeten <chraetenlategan@gmail.com>
2026-02-22 10:04:21 +0200

    Add Scoreboard class for game scoring

---
+++ Day23 scoreboard.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.update_scoreboard()
+    
+    
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(0,350)
+        self.write("Level", align = "center",font=("Arial",25,"normal"))
